121ade21ccf2b0de4b521d16fd2232f1db14f4dd
JhomaroGC <empresariar@gmail.com>
2021-06-19 21:51:05 -0500

    2021-06-19 22:00

--- modelling/app.py
+++ modelling/app.py
@@ -1,6 +1,6 @@
 from data import PriceHousesData
-from model import PriceHouses_model
-from data import _load_read_data
+from model import PriceHouses_model, save_submission
+
 
 #Get sets for train an test, and df_ for save submissiion
 X_train, X_test, y_train, df_submission = PriceHousesData("data\house-prices-advanced-regression-techniques.zip", "train.csv", "test.csv").preprocessing_all_data()
@@ -8,5 +8,6 @@ X_train, X_test, y_train, df_submission = PriceHousesData("data\house-prices-adv
 # Fit model with gridsearchCV
 predictions = PriceHouses_model(X_train, y_train, X_test).fit_model_grid_searchCV()
 
-
-
+#save submission
+submission = save_submission()
+print(submission)
--- modelling/model.py
+++ modelling/model.py
@@ -1,3 +1,4 @@
+from pandas.io.parsers import read_csv
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
@@ -9,16 +10,24 @@ class PriceHouses_model():
         self.X_test = X_test
     
     def fit_model_grid_searchCV(self):
-        parameters = {"n_estimators": [1,2],
+        parameters = {"n_estimators": [30,50,100],
                       "criterion": ["mse", "mae"],
-                      "max_depth": [1, 2, 3]}
+                      "max_depth": [5, 10, 15]}
         rfr = RandomForestRegressor()
         clf = GridSearchCV(rfr, parameters)
         clf.fit(self.X_train, self.y_train)
-        print(f"Mejores parámetros del modelo después de la búsqueda: {clf.best_params_}")
+        # print(f"Mejores parámetros del modelo después de la búsqueda: {clf.best_params_}")
         predicts = clf.predict(self.X_test)
-        preds = pd.Dataframe(predicts)
-        preds.to_csv(".\outputs\\preds.txt")
-        print("Predicciones del modelo: \n", predicts)
+        preds = pd.DataFrame(predicts)
+        preds.to_csv(".\outputs\\preds.csv")
         return predicts
     
+def save_submission():
+    test = pd.read_csv("data\\test.csv")
+    preds = pd.read_csv("outputs\\preds.csv")
+    submission = pd.concat([test['Id'], preds], axis = 1)
+    sub = pd.DataFrame(submission.values, columns = ['Id', 'Id_', 'SalePrice'])
+    sub = sub.set_index("Id")
+    sub[['SalePrice']].to_csv("outputs\\my_submission.csv")
+    print("Predicción guardada exitosamente, lista para envío: \n")
+    return sub[["SalePrice"]]
