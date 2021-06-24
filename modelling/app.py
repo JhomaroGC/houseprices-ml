c975eda31eccbe63bf17d715c3f96a699fbb2c5e
JhomaroGC <empresariar@gmail.com>
2021-06-24 18:42:30 -0500

    2021-06-24 19:00

--- modelling/app.py
+++ modelling/app.py
@@ -1,13 +1,13 @@
 from data import PriceHousesData
-from model import PriceHouses_model, save_submission
+from model_1 import PriceHouses_model, save_submission
 
 
 #Get sets for train an test, and df_ for save submissiion
 X_train, X_test, y_train, df_submission = PriceHousesData("data\house-prices-advanced-regression-techniques.zip", "train.csv", "test.csv").preprocessing_all_data()
 
-# Fit model with gridsearchCV
-predictions = PriceHouses_model(X_train, y_train, X_test).fit_model_grid_searchCV()
+# Fit model_1 with gridsearchCV
+# predictions = PriceHouses_model(X_train, y_train, X_test).fit_model_grid_searchCV()
 
 #save submission
-submission = save_submission()
-print(submission)
+# submission = save_submission()
+# print(submission)
